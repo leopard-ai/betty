ba5eaee55df44dc6e2e63b12a30265835155560d
Sang Choe <sangkeuc@andrew.cmu.edu>
2022-06-20 21:34:22 -0400

    memory optimization tutorial update

--- tutorial/4_memory_optimization.py
+++ tutorial/4_memory_optimization.py
@@ -11,18 +11,32 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.data import DataLoader
 from torchvision import transforms
-from torchvision.datasets import MNIST
+from torchvision.datasets import CIFAR10
 
 from betty.engine import Engine
 from betty.problems import ImplicitProblem
 from betty.configs import Config, EngineConfig
 
 
+fp16 = True
+distributed = True
+
+
 def build_dataset(reweight_size=1000, imbalanced_factor=100):
-    transform = transforms.Compose(
-        [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
+    normalize = transforms.Normalize(
+        mean=[x / 255.0 for x in [125.3, 123.0, 113.9]],
+        std=[x / 255.0 for x in [63.0, 62.1, 66.7]],
     )
-    dataset = MNIST(root="./data", train=True, download=True, transform=transform)
+    transform = transforms.Compose(
+        [
+            transforms.RandomCrop(32, padding=4, padding_mode="reflect"),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            normalize,
+        ]
+    )
+
+    dataset = CIFAR10(root="./data", train=True, download=True, transform=transform)
 
     num_classes = len(dataset.classes)
     num_meta = int(reweight_size / num_classes)
@@ -59,8 +73,12 @@ def build_dataset(reweight_size=1000, imbalanced_factor=100):
 
 
 classifier_dataset, reweight_dataset = build_dataset(reweight_size=1000, imbalanced_factor=100)
-transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
-valid_dataset = MNIST(root="./data", train=False, transform=transform)
+normalize = transforms.Normalize(
+    mean=[x / 255.0 for x in [125.3, 123.0, 113.9]],
+    std=[x / 255.0 for x in [63.0, 62.1, 66.7]],
+)
+valid_transform = transforms.Compose([transforms.ToTensor(), normalize])
+valid_dataset = CIFAR10(root="./data", train=False, transform=valid_transform)
 valid_dataloader = DataLoader(valid_dataset, batch_size=100, pin_memory=True)
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -84,7 +102,7 @@ class Reweight(ImplicitProblem):
         return {"loss": loss, "acc": acc}
 
 
-Config(type="darts", log_step=100, fp16=True)
+reweight_config = Config(type="darts", log_step=100, fp16=fp16)
 reweight = Reweight(
     name="reweight",
     module=reweight_module,
@@ -100,8 +118,77 @@ reweight = Reweight(
 classifier_dataloader = DataLoader(
     classifier_dataset, batch_size=100, shuffle=True, pin_memory=True
 )
-classifier_module = nn.Sequential(nn.Flatten(), nn.Linear(784, 200), nn.ReLU(), nn.Linear(200, 10))
-classifier_optimizer = optim.SGD(classifier_module.parameters(), lr=0.1, momentum=0.9)
+
+
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(Bottleneck, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, self.expansion * planes, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(self.expansion * planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion * planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(
+                    in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False
+                ),
+                nn.BatchNorm2d(self.expansion * planes),
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = F.relu(self.bn2(self.conv2(out)))
+        out = self.bn3(self.conv3(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+
+class ResNet(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=10):
+        super(ResNet, self).__init__()
+        self.in_planes = 64
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.linear = nn.Linear(512 * block.expansion, num_classes)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1] * (num_blocks - 1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = F.avg_pool2d(out, 4)
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return out
+
+
+def ResNet50():
+    return ResNet(Bottleneck, [3, 4, 6, 3])
+
+
+classifier_module = ResNet50()
+classifier_optimizer = optim.SGD(classifier_module.parameters(), lr=0.01, momentum=0.9)
 classifier_scheduler = optim.lr_scheduler.MultiStepLR(
     classifier_optimizer, milestones=[1500, 2500], gamma=0.1
 )
@@ -118,7 +205,7 @@ class Classifier(ImplicitProblem):
         return torch.mean(weight * loss_reshape)
 
 
-classifier_config = Config(unroll_steps=1)
+classifier_config = Config(unroll_steps=1, fp16=fp16)
 classifier = Classifier(
     name="classifier",
     module=classifier_module,
@@ -148,7 +235,8 @@ class ReweightingEngine(Engine):
 
         return {"acc": acc, "best_acc": self.best_acc}
 
-engine_config = EngineConfig(train_iters=3000, valid_step=100, distributed=True)
+
+engine_config = EngineConfig(train_iters=3000, valid_step=100, distributed=distributed)
 
 problems = [reweight, classifier]
 u2l = {reweight: [classifier]}
