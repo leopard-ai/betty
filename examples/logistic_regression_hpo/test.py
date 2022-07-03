70166e71e9b7d722efff3011ae6b959a559a3ba1
Sang Choe <sangkeuc@andrew.cmu.edu>
2022-07-02 23:36:38 -0400

    fix test

--- examples/logistic_regression_hpo/test.py
+++ examples/logistic_regression_hpo/test.py
@@ -117,6 +117,6 @@ u2l = {parent: [child]}
 l2u = {child: [parent]}
 dependencies = {"l2u": l2u, "u2l": u2l}
 
-engine = Engine(config=None, problems=problems, dependencies=dependencies)
+engine = Engine(config=engine_config, problems=problems, dependencies=dependencies)
 engine.run()
 print("*** Hello (Betty) World ***")
