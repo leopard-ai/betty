6426d4a830f7b53d5e31bf01012aa2f340b5aec3
willieneis <willie.neiswanger@gmail.com>
2022-07-12 01:44:15 -0700

    black format docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -92,8 +92,7 @@ html_title = "Betty"
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-html_theme_options = {
-}
+html_theme_options = {}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
