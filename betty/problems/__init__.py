d3ea23d20683e64fa18dde23c90b5cf0afb0ff59
Sang Choe <sangkeuc@andrew.cmu.edu>
2022-10-07 10:02:27 -0400

    remove meta_iterative

--- betty/problems/__init__.py
+++ betty/problems/__init__.py
@@ -2,4 +2,3 @@ from .problem import Problem
 from .implicit_problem import ImplicitProblem
 from .iterative_problem import IterativeProblem
 from .iterative_problem_higher import HigherIterativeProblem
-from .iterative_problem_meta import MetaIterativeProblem
