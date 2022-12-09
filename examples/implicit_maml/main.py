66864af7f1311b5f4162719628047ea6caade54d
Sang Choe <sangkeuc@andrew.cmu.edu>
2022-12-09 12:08:51 -0500

    black formatting

--- examples/implicit_maml/main.py
+++ examples/implicit_maml/main.py
@@ -207,7 +207,8 @@ parent = Outer(
     device=device,
 )
 children = [
-    Inner(name=f"inner_{i}", config=child_config, device=device) for i in range(arg.task_num)
+    Inner(name=f"inner_{i}", config=child_config, device=device)
+    for i in range(arg.task_num)
 ]
 env = MAMLEnv()
 problems = children + [parent]
