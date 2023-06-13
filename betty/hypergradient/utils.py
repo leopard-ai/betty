1f7e565374b8f931dda4ee5b700625e5afa6b829
Sang Choe <sangkeuc@andrew.cmu.edu>
2023-06-13 12:35:28 -0400

    modularize sama preconditioning

--- betty/hypergradient/utils.py
+++ betty/hypergradient/utils.py
@@ -5,41 +5,68 @@ def get_optimzer_type(optimizer):
     cls_name = type(optimizer).__name__.lower()
     if "adam" in cls_name:
         return "adam"
+    elif "rmsprop" in cls_name:
+        return "rmsprop"
     return "sgd"
+
+
+def precondition_sgd(vectors, problem):
+    return vectors
+
+
+def precondition_adam(vectors, problem):
+    outputs = []
+
+    params = problem.meta_trainable_parameters()
+    for vector, param in zip(vectors, params):
+        param_group = problem.get_opt_param_group_for_param(param)
+        state = problem.get_opt_state_for_param(param)
+
+        with torch.no_grad():
+            beta1, beta2 = param_group["betas"]
+            eps = param_group["eps"]
+            last_grad = state.get("last_grad", torch.zeros_like(vector))
+            exp_avg = state.get("exp_avg", torch.zeros_like(vector))
+            exp_avg_sq = state.get("exp_avg_sq", torch.zeros_like(vector))
+            exp_avg_old = (
+                (exp_avg - (1 - beta1) * last_grad) / beta1 if beta1 != 0 else 0
+            )
+            exp_avg_sq_old = (exp_avg_sq - (1 - beta2) * last_grad * last_grad) / beta2
+
+            scale = (1 - beta1) * beta2 * exp_avg_sq_old - beta1 * (
+                1 - beta2
+            ) * last_grad * exp_avg_old
+            scale /= (torch.sqrt(exp_avg_sq) + eps) ** 3
+        out = vector * scale * param_group["lr"]
+        outputs.append(out)
+
+    return outputs
+
+
+def precondition_rmsprop(vectors, problem):
+    raise NotImplementedError("SAMA preconditioning for RMSProp is not implemented!")
+
+
+def precondition_adagrad(vectors, problem):
+    raise NotImplementedError("SAMA preconditioning for Adagrad is not implemented!")
+
+
+def precondition_adadelta(vectors, problem):
+    raise NotImplementedError("SAMA preconditioning for Adadelta is not implemented!")
+
+
+precondition_fn_mapping = {
+    "sgd": precondition_sgd,
+    "adam": precondition_adam,
+    "rmsprop": precondition_rmsprop,
+    "adagrad": precondition_adagrad,
+    "adadelta": precondition_adadelta,
+}
 
 
 def precondition(vectors, problem):
     optimizer = problem.optimizer
     optimizer_type = get_optimzer_type(optimizer)
+    precondition_fn = precondition_fn_mapping[optimizer_type]
 
-    if optimizer_type == "sgd":
-        return vectors
-
-    outputs = []
-    if optimizer_type == "adam":
-        params = problem.meta_trainable_parameters()
-        for vector, param in zip(vectors, params):
-            param_group = problem.get_opt_param_group_for_param(param)
-            state = problem.get_opt_state_for_param(param)
-
-            with torch.no_grad():
-                beta1, beta2 = param_group["betas"]
-                eps = param_group["eps"]
-                last_grad = state.get("last_grad", torch.zeros_like(vector))
-                exp_avg = state.get("exp_avg", torch.zeros_like(vector))
-                exp_avg_sq = state.get("exp_avg_sq", torch.zeros_like(vector))
-                exp_avg_old = (
-                    (exp_avg - (1 - beta1) * last_grad) / beta1 if beta1 != 0 else 0
-                )
-                exp_avg_sq_old = (
-                    exp_avg_sq - (1 - beta2) * last_grad * last_grad
-                ) / beta2
-
-                scale = (1 - beta1) * beta2 * exp_avg_sq_old - beta1 * (
-                    1 - beta2
-                ) * last_grad * exp_avg_old
-                scale /= (torch.sqrt(exp_avg_sq) + eps) ** 3
-            out = vector * scale * param_group["lr"]
-            outputs.append(out)
-
-    return outputs
+    return precondition_fn(vectors, problem)
